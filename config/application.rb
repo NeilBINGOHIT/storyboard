648186ae2bba41a9f2768834b56d8f753bfe38df
NeilBINGOHIT <neiljiang0813@gmail.com>
2015-02-24 03:58:22 -0500

    modified:   config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Storyboard
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
+    
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
